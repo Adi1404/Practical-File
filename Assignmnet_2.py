da9e9fd6481b8ca7281ad253fed541b7cf148f7d
Adi1404 <101738157+Adi1404@users.noreply.github.com>
2022-04-15 10:24:20 +0530

    Create Assignmnet_2.py

---
+++ Assignmnet_2.py
@@ -0,0 +1,12 @@
+#20CE077 Aditya Patel
+num = list(map(int, input("Enter numbers : ").split()))
+import numpy
+import statistics
+
+def Average(lst):
+    return sum(lst) / len(lst)
+print("Minimum : " + str(min(num)))
+print("Maximum : " + str(max(num)))
+print("Average : " + str(Average(num)))
+print("Standard Deviation : " + str(statistics.stdev(num)))
+print("Vartiance : " + str(numpy.var(num)))
